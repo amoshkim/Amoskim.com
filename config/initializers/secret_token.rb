80ce994548834c7cb808cc24fe70706945e81111
Amos <amos@a.com>
2014-10-24 15:09:35 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amoskim::Application.config.secret_key_base = '134ccdb588f4c45061cdf2f1bccfc4c3343365778adc8e2a9d6337747c0da1615c61e799eaf4ba44ae8f9100a42a251191809e1577f7f6ecf272ff78cd75a204'
